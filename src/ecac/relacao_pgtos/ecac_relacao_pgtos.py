7ffeacfc656dd6f70339336d3eee0cd43b43036a
luangrabher <luanggcontato@gmail.com>
2024-04-26 20:27:06 -0300

    feat - login no ecac funcional com recaptcha

--- src/ecac/relacao_pgtos/ecac_relacao_pgtos.py
+++ src/ecac/relacao_pgtos/ecac_relacao_pgtos.py
@@ -1,46 +1,61 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from easygui import msgbox
+import undetected_chromedriver as uc
+
 
 url_ecac = 'https://cav.receita.fazenda.gov.br/'
 driver = None
 
+
 def ecac_get_relacao_pgtos():
     relacao = RelacaoPgtos()
-    
+
     return relacao.get_relacao_pgtos()
 
+
 class RelacaoPgtos:
-    
+
     url_ecac = 'https://cav.receita.fazenda.gov.br/'
     driver = None
-    
+
     def __init__(self):
-        self.driver = webdriver.Chrome()
-        
+        self.driver = uc.Chrome()
+
     def get_relacao_pgtos(self):
-        self.login()
+        if not self.login():
+            return False
+
         
-        pass
-    
+
+        self.driver.close()
+
     def login(self):
-        url = self.url_ecac + 'autenticacao/login'
-        
-        self.driver.get(url)
-        
-        selector_botao_fazer_login = '#login-dados-certificado input[onclick*="validarHc'
-        self.driver.find_element(By.CSS_SELECTOR, selector_botao_fazer_login).click()
-        
-        
-        selector_botao_selecionar_certificado = 'button#login-certificate'
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, selector_botao_selecionar_certificado)))
-        self.driver.find_element(By.CSS_SELECTOR, selector_botao_selecionar_certificado).click()
-        
-        msgbox('Resolva o captcha e selecione o certificado digital')
-        
-        pass
+        try:
+            url = self.url_ecac + 'autenticacao/login'
+
+            self.driver.get(url)
+
+            selector_botao_fazer_login = '#login-dados-certificado input[onclick*="validarHc'
+            self.driver.find_element(
+                By.CSS_SELECTOR, selector_botao_fazer_login).click()
+
+            selector_botao_selecionar_certificado = 'button#login-certificate'
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, selector_botao_selecionar_certificado)))
+            self.driver.find_element(
+                By.CSS_SELECTOR, selector_botao_selecionar_certificado).click()
+
+            msgbox('Resolva o captcha caso apareça e selecione o certificado digital')
+
+            WebDriverWait(self.driver, 10).until(
+                EC.url_contains('cav.receita.fazenda.gov.br/ecac'))
+        except:
+            msgbox('Erro ao tentar fazer login no ECAC')
+            return False
+
+        return True
 
 
 if __name__ == '__main__':
