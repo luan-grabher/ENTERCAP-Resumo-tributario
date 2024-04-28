eb0178858281b6c16b377de009a0d0df1cd46eb2
luangrabher <luanggcontato@gmail.com>
2024-04-28 14:09:44 -0300

    feat - extratos de compras do sefaz

--- src/sefaz/compras/compras.py
+++ src/sefaz/compras/compras.py
@@ -2,15 +2,15 @@
 from src.sefaz.extratosNotas.extratosNotas import TIPOS_OPERACAO, get_sefaz_extrato_notas
 from src.sefaz.sefaz import get_driver_sefaz_logado
 
-def get_faturamento_sefaz(driver, cnpj, anos: list):
-    faturamento = get_sefaz_extrato_notas(driver, cnpj, anos, TIPOS_OPERACAO['emissao'])
+def get_compras_sefaz(driver, cnpj, anos: list):
+    compras = get_sefaz_extrato_notas(driver, cnpj, anos, TIPOS_OPERACAO['recebimento'])
 
 if __name__ == '__main__':
     driver = get_driver_sefaz_logado()
     if driver:
         cnpj = '46.540.315/0003-94'        
         anos = [2019, 2020, 2021, 2022, 2023]
-        faturamento = get_faturamento_sefaz(driver, cnpj, anos)
-        print(faturamento)
+        compras = get_compras_sefaz(driver, cnpj, anos)
+        print(compras)
         
         driver.close()
