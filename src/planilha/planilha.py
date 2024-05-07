99d53b3aae0245b21e8d7e53604463e0cc46c15d
luangrabher <luanggcontato@gmail.com>
2024-05-07 10:41:00 -0300

    feat - atualizado template - planilha agora utiliza lib openpyxl

--- src/planilha/planilha.py
+++ src/planilha/planilha.py
@@ -1,4 +1,5 @@
 import xlsxwriter
+import openpyxl
 
 def get_number_for_letter(letter):
     normalized_letter = letter.upper()
@@ -9,7 +10,23 @@ class Planilha:
         self.arquivo = arquivo
         self.workbook = xlsxwriter.Workbook(self.arquivo)
 
+        
+    def set_CNPJ(self, cnpj):
+        aba = self.workbook.get_worksheet_by_name('Apresentação')
+        if aba is None:
+            return False
+        
+        aba.write('D7', 'CNPJ: ' + cnpj)
+        
+    def save(self):
+        self.workbook.close()
+
 if __name__ == '__main__':
-    planilha = Planilha('template_apresentacao.xlsx')
+    planilha_path = 'template.xlsx'
     
-    print(planilha.workbook)
+    planilha = openpyxl.load_workbook(planilha_path)
+    
+    planilha['Apresentação']['D7'] = 'CNPJ: 12345678901234'
+    planilha['Apresentação']['B7'] = 'EMPRESA: Teste'
+    
+    planilha.save('output.xlsx')
