4495f7435015d0cdf0faf66ea43ffa7a28ce4245
luangrabher <luanggcontato@gmail.com>
2024-05-16 10:38:06 -0300

    feat - aba dados na planilha. - funcao que insere as colunas de mes na aba de dados

--- src/planilha/planilha.py
+++ src/planilha/planilha.py
@@ -18,6 +18,39 @@ class Planilha:
             
     def save(self, output_path = 'output.xlsx'):
         self.workbook.save(output_path)
+    
+    def inserir_colunas_mes_aba_dados(self, mesInicio: int, anoInicio: int, mesFim: int, anoFim: int):
+        aba_dados = self.workbook['Dados']
+        
+        coluna_inicio = get_number_for_letter('B')
+        
+        mesesstr = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+        
+        for ano in range(anoInicio, anoFim + 1):
+            for mes in range(mesInicio, mesFim + 1):
+                coluna = coluna_inicio + (ano - anoInicio) * 12 + mes - mesInicio
+                aba_dados.insert_cols(coluna)
+                
+                messtr = mesesstr[mes - 1]
+                aba_dados.cell(row=1, column=coluna, value=f'{messtr}/{ano}')
+        
+        self.ajustar_width_colunas_aba('Dados')
+                
+    
+    def ajustar_width_colunas_aba(self, nome_aba):
+        aba = self.workbook[nome_aba]
+        
+        for col in aba.columns:
+            max_length = 0
+            column = col[0].column_letter
+            for cell in col:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(cell.value)
+                except:
+                    pass
+            adjusted_width = (max_length + 2)
+            aba.column_dimensions[column].width = adjusted_width
 
 if __name__ == '__main__':
     planilha_path = 'template.xlsx'
@@ -25,5 +58,6 @@ if __name__ == '__main__':
     planilha = Planilha(planilha_path)
     planilha.set_CNPJ('123456789')
     planilha.set_EMPRESA('Empresa ABC')
+    planilha.inserir_colunas_mes_aba_dados(1, 2021, 12, 2023)
     
     planilha.save('output.xlsx')
