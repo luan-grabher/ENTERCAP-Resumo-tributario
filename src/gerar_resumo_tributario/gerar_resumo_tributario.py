729a25f16e0bf11f502ff72a7b8a2fff72383d3b
luangrabher <luanggcontato@gmail.com>
2024-05-20 13:34:32 -0300

    bugfix - corrigido normalizacao cnpj para path do arquivo output

--- src/gerar_resumo_tributario/gerar_resumo_tributario.py
+++ src/gerar_resumo_tributario/gerar_resumo_tributario.py
@@ -1,4 +1,5 @@
 import os
+import re
 from easygui import msgbox
 from src.ecac.ecac import get_driver_ecac_logado
 from src.ecac.pgdas.ecac_pgdas import get_pgdas, limpar_downloads_pgdas
@@ -19,45 +20,55 @@ def gerar_resumo_tributario(cnpj, anos):
         msgbox('Erro ao tentar fazer login no eSocial')
         return False
     
-    anos = sorted(anos, reverse=True)
-    ano_final = anos[0]
-    ano_inicial = anos[-1]
-    
-    data_inicial = f'01/01/{ano_inicial}'
-    data_final = f'31/12/{ano_final}'
-    
+    try:
+        anos = sorted(anos, reverse=True)
+        ano_final = anos[0]
+        ano_inicial = anos[-1]
         
-    relacao_pgtos, total_pgtos = ecac_get_relacao_pgtos(driver, data_inicial=data_inicial, data_final=data_final)
-    relacao_pgtos_para_planilha = converter_relacao_pgtos_lista_planilha(relacao_pgtos)
-    
-    limpar_downloads_pgdas()
-    das_para_planilha = get_pgdas(driver, anos)
-    limpar_downloads_pgdas()
-    
-    contribuicao_folha = get_contribuicao_folha(driver, anos)    
-    
-    template_path = 'template.xlsx'
-    
-    planilha = Planilha(template_path)
-    planilha.set_CNPJ(cnpj)
-    planilha.set_EMPRESA('Empresa ABC LTDA') # TODO: desenvolver get_razao_social(cnpj)
-    
-    planilha.inserir_colunas_mes_aba_dados(1, int(ano_inicial), 12, int(ano_final))
-    planilha.insert_dados_aba_dados(relacao_pgtos_para_planilha, True)
-    planilha.insert_dados_aba_dados(das_para_planilha, True)
-    
-    planilha.insert_dados_aba_dados(contribuicao_folha['base_calculo'], False)
-    planilha.inserir_valor_dado_na_apresentacao_pela_descricao('FOLHA (Total Período)', contribuicao_folha['base_calculo']['descricao'])
-    
-    planilha.insert_dados_aba_dados(contribuicao_folha['valor_contribuicao'], True)
-    
-    cnpj_numeros = cnpj.replace(r'\D', '')
-    desktop_path = os.path.expanduser('~') + '\\Desktop'
-    output_path = f'{desktop_path}\\{cnpj_numeros} resumo tributario {ano_inicial}_{ano_final}.xlsx'
-    planilha.save(output_path)
+        data_inicial = f'01/01/{ano_inicial}'
+        data_final = f'31/12/{ano_final}'
+        
+            
+        relacao_pgtos, total_pgtos = ecac_get_relacao_pgtos(driver, data_inicial=data_inicial, data_final=data_final)
+        relacao_pgtos_para_planilha = converter_relacao_pgtos_lista_planilha(relacao_pgtos)
+        
+        limpar_downloads_pgdas()
+        das_para_planilha = get_pgdas(driver, anos)
+        limpar_downloads_pgdas()
+        
+        contribuicao_folha = get_contribuicao_folha(driver, anos)    
+        
+        template_path = 'template.xlsx'
+        
+        planilha = Planilha(template_path)
+        planilha.set_CNPJ(cnpj)
+        planilha.set_EMPRESA('Empresa ABC LTDA') # TODO: desenvolver get_razao_social(cnpj)
+        
+        planilha.inserir_colunas_mes_aba_dados(1, int(ano_inicial), 12, int(ano_final))
+        planilha.insert_dados_aba_dados(relacao_pgtos_para_planilha, True)
+        planilha.insert_dados_aba_dados(das_para_planilha, True)
+        
+        planilha.insert_dados_aba_dados(contribuicao_folha['base_calculo'], False)
+        planilha.inserir_valor_dado_na_apresentacao_pela_descricao('FOLHA (Total Período)', contribuicao_folha['base_calculo'][0]['descricao'])
+        
+        planilha.insert_dados_aba_dados(contribuicao_folha['valor_contribuicao'], True)
+        
+        cnpj_numeros = re.sub(r'\D', '', cnpj)
+        desktop_path = os.path.expanduser('~') + '\\Desktop'
+        output_path = f'{desktop_path}\\{cnpj_numeros} resumo tributario {ano_inicial}_{ano_final}.xlsx'
+        planilha.save(output_path)
+        
+        msgbox(f'Planilha gerada com sucesso em {output_path}')
+        
+    except Exception as e:
+        print('Erro ao gerar resumo tributário:', e)
+        msgbox('Erro ao gerar resumo tributário')
+        
+    driver.quit()
 
 if __name__ == '__main__':
     cnpj = '46.540.315/0001-22'
+    
     anos = ['2023', '2022', '2021']
     
     gerar_resumo_tributario(cnpj, anos)
