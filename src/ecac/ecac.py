b02faa0d2117247682ea928ef57bea0b1fac1662
luangrabher <luanggcontato@gmail.com>
2024-05-20 10:39:10 -0300

    feat - tratamento de erros no login ecac

--- src/ecac/ecac.py
+++ src/ecac/ecac.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,28 +10,42 @@ def get_driver_ecac_logado(driver=None):
         url = 'https://cav.receita.fazenda.gov.br/autenticacao/login'        
         
         if not driver:
+            print('Driver não informado, criando novo driver')
             driver = uc.Chrome()
 
         driver.get(url)
 
-        selector_botao_fazer_login = '#login-dados-certificado input[onclick*="validarHc'
-        driver.find_element(
-            By.CSS_SELECTOR, selector_botao_fazer_login).click()
+        try:
+            selector_botao_fazer_login = '#login-dados-certificado input[onclick*="validarHc'
+            element = driver.find_element(By.CSS_SELECTOR, selector_botao_fazer_login)
+            element.click()
+            
+            #click by js
+            driver.execute_script("arguments[0];", element)
+            driver.execute_script("arguments[0].click();", element)
+        except:
+            raise Exception('Botão de login com certificado não encontrado')
 
-        selector_botao_selecionar_certificado = 'button#login-certificate'
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-            (By.CSS_SELECTOR, selector_botao_selecionar_certificado)))
-        driver.find_element(
-            By.CSS_SELECTOR, selector_botao_selecionar_certificado).click()
+        try:
+            selector_botao_selecionar_certificado = 'button#login-certificate'
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, selector_botao_selecionar_certificado)))
+            driver.find_element(
+                By.CSS_SELECTOR, selector_botao_selecionar_certificado).click()
+        except:
+            raise Exception('Botão de selecionar certificado não encontrado')
 
         msgbox('Resolva o captcha caso apareça e selecione o certificado digital')
 
-        WebDriverWait(driver, 10).until(
-            EC.url_contains('cav.receita.fazenda.gov.br/ecac'))
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.url_contains('cav.receita.fazenda.gov.br/ecac'))
+        except:
+            raise Exception('Não fez o redirect correto do login')
         
         return driver
     except Exception as e:
-        print(e)
+        print("(ECAC) Erro ao tentar fazer login no ECAC:", e)
         msgbox('Erro ao tentar fazer login no ECAC')
         return False    
 
